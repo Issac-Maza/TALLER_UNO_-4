c2d223d4461245c31cfd7b5e2c24e66f3e50c875
mamomeri <77068238+mamomeri@users.noreply.github.com>
2024-11-03 12:43:12 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <ctype.h>  
 #include "datos.h"
 #include "calculos.h"
 
@@ -6,35 +7,40 @@ int main() {
     int figura;
 
     do {
-        mostrarOpciones();
-	printf("Ingrese subiamente");
-	scanf("%d", &figura);
+        mostrarMenu();
+        printf("Ingrese una opción: ");
+        scanf("%d", &figura);
 
-	if(opcion >=1 || opcion<=13){
-        	solicitarDatos(figura);
-	}else if(opcion ==0 ){
-		printf("Saliendo del programa.\n");
-	}else{
-		printf("Opcion no valida.Intente de nuevo.\n");
-	}
+        switch (figura) {
+            case 1 ... 13:
+                solicitarDatosFigura(figura);
+                break;
+            case 0:
+                printf("Saliendo del programa.\n");
+                break;
+            default:
+                printf("Opción no válida. Intente de nuevo.\n");
+                break;
+        }
 
+        if (figura != 0) {
+            char respuesta;
+            do {
+                printf("¿Desea realizar el análisis de otra figura geométrica? (s/n): ");
+                scanf(" %c", &respuesta);
+                respuesta = tolower(respuesta);  // Convertir a minúscula
 
-	if (opcion != 0) {
-    		char respuesta;
-    		do {
-        		printf("¿Desea realizar el análisis de otra figura geométrica? (s/n): ");
-        		scanf(" %c", &respuesta);
-        		if (respuesta == 'n' || respuesta == 'N') {
-            			opcion = 0;  // Salir del bucle
-        		} else if (respuesta == 's' || respuesta == 'S') {
-            			break;  // Continuar con otro análisis
-        		} else {
-            			printf("Respuesta no válida. Por favor, ingrese 's' para sí o 'n' para no.\n");
-        		}
-    		} while (respuesta != 's' && respuesta != 'S' && respuesta != 'n' && respuesta != 'N');
-	}
+                if (respuesta == 'n') {
+                    figura = 0;  // Salir del bucle
+                } else if (respuesta == 's') {
+                    break;  // Continuar con otro análisis
+                } else {
+                    printf("Respuesta no válida. Por favor, ingrese 's' para sí o 'n' para no.\n");
+                }
+            } while (respuesta != 's' && respuesta != 'n');
+        }
 
-    } while(opcion !=0);
+    } while (figura != 0);
 
     return 0;
 }
